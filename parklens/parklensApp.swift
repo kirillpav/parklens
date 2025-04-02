86bfa50c461abaf07b3a69901033772c3d983beb
kirill pavlov <2020kirillp@gmail.com>
2025-04-02 09:33:52 -0700

    Initial Commit

---
+++ parklens/parklensApp.swift
@@ -0,0 +1,17 @@
+//
+//  parklensApp.swift
+//  parklens
+//
+//  Created by Kirill Pavlov on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct parklensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ parklensTests/parklensTests.swift
@@ -0,0 +1,17 @@
+//
+//  parklensTests.swift
+//  parklensTests
+//
+//  Created by Kirill Pavlov on 4/2/25.
+//
+
+import Testing
+@testable import parklens
+
+struct parklensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
